d7faff085dd30255450ac1316d0489e6e9da2e96
hbaltz <hugo.baltz@ensg.eu>
2016-12-08 21:41:29 +0100

    Report

--- Doc/rapport/Annexes/pandoc/pandoc.sh
+++ Doc/rapport/Annexes/pandoc/pandoc.sh
@@ -4,6 +4,6 @@ pandoc -N \
        --variable documentclass=article \
        --variable fontsize=12pt \
        --variable papersize=a4paper \
-       --variable version=0.2.0 \
+       --variable version=1.0.0 \
        --template=pandoc/template.tex \
        *.yaml *.md -o ../annexe.pdf
